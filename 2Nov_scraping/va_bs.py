0b332e2b1cdb6b8d6fdf63bd42e1f59155974d44
Yuxi Wu <yuxiwu@uchicago.edu>
2017-11-02 14:42:29 -0500

    notes from TA session 2/11

---
+++ 2Nov_scraping/va_bs.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+
+url_va = 'http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2016/office_id:1/stage:General'
+
+req_va = requests.get(url_va)
+html_va = req_va.content
+soup = BeautifulSoup(html_va,'html.parser')
+
+#find 'tr' tags with class 'election_item because these contain the ID'
+tags = soup.find_all('tr','election_item')
+
+# if you just want to look at 2016's data
+lastyear = tags[0]
+lastyear_id = lastyear['id'] #text of id
+lastyear_id = lastyear['id'][-5:] #you only want the last five characters
+
+#printing all of the year/id pairs
+for t in tags:
+    year = t.td.text
+    year_id = t['id'][-5:]
+    print(year, year_id)
+#...now do the same thing, but put it all in a list called ELECTION_ID
+#maybe you can do this with a list comprehension!
+
+#the base URL that all downloads pages have
+base = 'http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/'
+
+#the url for 2016
+lastyear_url = base.format('80871')
+lastyear_text = requests.get(lastyear_url).text
+
+#writing 2016 to a csv
+with open('2016.csv', 'w') as output:
+    output.write(requests.get(lastyear_url).text)
