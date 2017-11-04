044716f0129ff604c9898dc514d3ae7eb22e74e8
Yuxi Wu <yuxiwu@uchicago.edu>
2017-11-04 12:23:53 -0500

    helpful comments

--- 2Nov_scraping/va_bs.py
+++ 2Nov_scraping/va_bs.py
@@ -3,14 +3,19 @@ import requests
 
 url_va = 'http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2016/office_id:1/stage:General'
 
+# just things you do every time
 req_va = requests.get(url_va)
-html_va = req_va.content
-soup = BeautifulSoup(html_va,'html.parser')
+html_va = req_va.content #getting the contents of the website
+soup = BeautifulSoup(html_va,'html.parser') #turning it into a soup object so you can manipulate in python
+
+#open the html of url_va (you can do this if you right click anywhere on page and select 'view page source'
+#search for '80871' when you look at the HTML of the page.
+#what kind of tag comes in front?:
 
 #find 'tr' tags with class 'election_item because these contain the ID'
 tags = soup.find_all('tr','election_item')
 
-# if you just want to look at 2016's data
+#if you just want to look at 2016's data
 lastyear = tags[0]
 lastyear_id = lastyear['id'] #text of id
 lastyear_id = lastyear['id'][-5:] #you only want the last five characters
